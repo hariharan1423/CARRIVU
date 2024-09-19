1d7e4b85472c24c1347badf0fb44b7aa668ae6fa
THIRUVENGADAM Hariharan <hariharan.thiruvengadam@rntbci.com>
2024-09-19 21:07:16 +0530

    Create login page

--- carRIVU/urls.py
+++ carRIVU/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('',views.login,name='login'),
+    path('home/',views.home,name='home')
 ]
