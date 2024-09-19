9ab9fdd638def4699754942c65dcdef385e48735
THIRUVENGADAM Hariharan <hariharan.thiruvengadam@rntbci.com>
2024-09-19 21:47:06 +0530

    add login view

--- carRIVU/urls.py
+++ carRIVU/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
     path('',views.login,name='login'),
     path('home/',views.home,name='home')
 ]
--- carRIVU/views.py
+++ carRIVU/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 
 def home(request):
     return render(request,"home.html")
+
+def login(request):
+    if request.method=='POST':
+        return redirect('home')
+    
+    return render(request,'login.html')
